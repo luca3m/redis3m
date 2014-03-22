2a2ffc4934200303d126c55d47d9739649e0f078
Luca Marturana <lucamarturana@gmail.com>
2014-03-22 20:00:33 +0100

    Fix compile issues

--- src/patterns/orm.cpp
+++ src/patterns/orm.cpp
@@ -2,5 +2,5 @@
 
 using namespace redis3m::patterns;
 
-script_exec orm::save_script("data/lua/save.lua", true);
-script_exec orm::remove_script("data/lua/remove.lua", true);
+script_exec orm::save_script(DATADIR "/lua/save.lua", true);
+script_exec orm::remove_script(DATADIR "/lua/remove.lua", true);
