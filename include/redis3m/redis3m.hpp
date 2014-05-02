139f8d3605f9a0f6ef4d08d93842116e91d3f559
Luca Marturana <lucamarturana@gmail.com>
2014-05-02 15:10:36 +0200

    Add include-all files

---
+++ include/redis3m/redis3m.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2014 Luca Marturana. All rights reserved.
+// Licensed under Apache 2.0, see LICENSE for details
+
+#pragma once
+
+// Base
+#include <redis3m/connection.h>
+#include <redis3m/connection_pool.h>
+#include <redis3m/simple_pool.h>
