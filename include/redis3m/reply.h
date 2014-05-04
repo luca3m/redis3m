8fafc529bc7116d06cb255a570c6fc1972b39333
Luca Marturana <lucamarturana@gmail.com>
2014-05-04 21:23:36 +0200

    Add conversion operator on reply

--- include/redis3m/reply.h
+++ include/redis3m/reply.h
@@ -10,56 +10,60 @@
 struct redisReply;
 
 namespace redis3m {
-    
-    class connection;
+
+class connection;
+/**
+ * @brief Represent a reply received from redis server
+ */
+class reply
+{
+public:
     /**
-     * @brief Represent a reply received from redis server
+     * @brief Define reply type
      */
-    class reply
+    enum type_t
     {
-    public:
-        /**
-         * @brief Define reply type
-         */
-        enum type_t
-        {
-            STRING = 1,
-            ARRAY = 2,
-            INTEGER = 3,
-            NIL = 4,
-            STATUS = 5,
-            ERROR = 6
-        };
-
-        /**
-         * @brief Type of reply, other field values are dependent of this
-         * @return
-         */
-        inline type_t type() const { return _type; }
-        /**
-         * @brief Returns string value if present, otherwise an empty string
-         * @return
-         */
-        inline const std::string& str() const { return _str; }
-        /**
-         * @brief Returns integer value if present, otherwise 0
-         * @return
-         */
-        inline long long integer() const { return _integer; }
-        /**
-         * @brief Returns a vector of sub-replies if present, otherwise an empty one
-         * @return
-         */
-        inline const std::vector<reply>& elements() const { return _elements; }
-
-    private:
-        reply(redisReply *reply);
-
-        type_t _type;
-        std::string _str;
-        long long _integer;
-        std::vector<reply> _elements;
-
-        friend class connection;
+        STRING = 1,
+        ARRAY = 2,
+        INTEGER = 3,
+        NIL = 4,
+        STATUS = 5,
+        ERROR = 6
     };
+
+    /**
+     * @brief Type of reply, other field values are dependent of this
+     * @return
+     */
+    inline type_t type() const { return _type; }
+    /**
+     * @brief Returns string value if present, otherwise an empty string
+     * @return
+     */
+    inline const std::string& str() const { return _str; }
+    /**
+     * @brief Returns integer value if present, otherwise 0
+     * @return
+     */
+    inline long long integer() const { return _integer; }
+    /**
+     * @brief Returns a vector of sub-replies if present, otherwise an empty one
+     * @return
+     */
+    inline const std::vector<reply>& elements() const { return _elements; }
+
+    inline operator const std::string&() const { return _str; }
+
+    inline operator long long() const { return _integer; }
+
+private:
+    reply(redisReply *reply);
+
+    type_t _type;
+    std::string _str;
+    long long _integer;
+    std::vector<reply> _elements;
+
+    friend class connection;
+};
 }
