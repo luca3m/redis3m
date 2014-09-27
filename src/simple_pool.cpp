ad626ac9ba5f8c14a8f05722327f76d92eaf116c
Luca Marturana <lucamarturana@gmail.com>
2014-09-27 11:52:48 +0200

    improve simple_pool

--- src/simple_pool.cpp
+++ src/simple_pool.cpp
@@ -2,7 +2,6 @@
 // Licensed under Apache 2.0, see LICENSE for details
 
 #include <redis3m/simple_pool.h>
-#include <boost/lexical_cast.hpp>
 #include <redis3m/command.h>
 
 using namespace redis3m;
@@ -12,16 +11,13 @@ connection::ptr_t simple_pool::get()
     connection::ptr_t ret;
 
     access_mutex.lock();
-    for (std::set<connection::ptr_t>::iterator it = connections.begin(); it != connections.end(); ++it)
+    std::set<connection::ptr_t>::iterator it = connections.begin();
+    if (it != connections.end())
     {
-        if (it->get()->is_valid())
-        {
-            ret = *it;
-            connections.erase(it);
-            break;
-        }
+        ret = *it;
         connections.erase(it);
     }
+
     access_mutex.unlock();
 
     if (!ret)
@@ -30,7 +26,7 @@ connection::ptr_t simple_pool::get()
         // Setup connections selecting db
         if (_database != 0)
         {
-            ret->run(command("SELECT")(boost::lexical_cast<std::string>(_database)));
+            ret->run(command("SELECT") << _database);
         }
     }
     return ret;
@@ -39,8 +35,11 @@ connection::ptr_t simple_pool::get()
 
 void simple_pool::put(connection::ptr_t conn)
 {
-    boost::unique_lock<boost::mutex> lock(access_mutex);
-    connections.insert(conn);
+    if (conn->is_valid())
+    {
+        boost::unique_lock<boost::mutex> lock(access_mutex);
+        connections.insert(conn);
+    }
 }
 
 simple_pool::simple_pool(const std::string &host, unsigned int port):
